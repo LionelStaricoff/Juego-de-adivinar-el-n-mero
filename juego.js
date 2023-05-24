166aab0a95fad9d0d485462db55926a84e93dadc
lionel staricoff <staricofflionel@hotmail.com>
2023-05-24 16:10:07 -0500

    primer commit

---
+++ juego.js
@@ -0,0 +1,18 @@
+function jugar(){
+   
+    let dato =parseInt( document.querySelector("#input").value );
+
+    let datoAleatoreo = Math.round( Math.random()*10);
+    let respuesta = document.querySelector("p");
+
+    if(dato == datoAleatoreo){
+     respuesta.innerHTML = "Ganaste!!!";
+    }else{
+        respuesta.innerHTML = "perdiste!! tu número es: "+ dato + " y el número era: "+datoAleatoreo;
+    }
+}
+
+let boton = document.querySelector("#button");
+
+
+boton.onclick = jugar;
